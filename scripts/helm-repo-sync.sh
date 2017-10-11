8aff5fcf2119fd42723ed4a05e4abdc9c9ce8efa
Aaron Schlesinger <arschles@gmail.com>
2017-10-11 15:21:28 -0700

    Merging on-disk charts with those already in the repo (#39)
    * Merging on-disk charts with those already in the repo
    
    So that old versioned are preserved
    
    * Fixing the helm repo sync script to crawl charts in the proper location

--- scripts/helm-repo-sync.sh
+++ scripts/helm-repo-sync.sh
@@ -8,27 +8,29 @@ fi
 #####
 # set up the repo dir, and package up all charts
 #####
+AZURE_STORAGE_CONTAINER=azure
 REPO_DIR=repo
 mkdir -p $REPO_DIR
 echo "entering $REPO_DIR"
 cd $REPO_DIR
-for dir in `ls ..`;do
-    if [ ! -f ../$dir/Chart.yaml ];then
+# download the existing repo's index.yaml so that we can merge it later
+echo "downloading existing index.yaml"
+az storage blob download --container-name $AZURE_STORAGE_CONTAINER --file index.yaml --name index.yaml
+for dir in `ls ../..`;do
+    if [ ! -f ../../$dir/Chart.yaml ];then
         echo "skipping $dir because it lacks a Chart.yaml file"
     else
         echo "packaging $dir"
-        helm dep update ../$dir
-        helm package ../$dir
+        helm dep update ../../$dir
+        helm package ../../$dir
     fi
 done
 
 #####
-# index the charts
+# index the charts, merging with the old index.yaml so charts are versioned
 #####
 REPO_URL=https://kubernetescharts.blob.core.windows.net/
-helm repo index --url $REPO_URL .
-AZURE_STORAGE_CONTAINER=azure
-helm repo index --url ${REPO_URL} .
+helm repo index --url $REPO_URL --merge index.yaml .
 
 #####
 # upload to Azure blob storage
