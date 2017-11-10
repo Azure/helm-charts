0255c8d94d1f1cfa915ae0a76be112c07faf0a6d
Aaron Schlesinger <aaschles@microsoft.com>
2017-11-10 12:04:41 -0800

    Modifying the install script for azure-cli
    This patch updates the apt key for the MSFT repository. It should fix
    some build failures on master

--- scripts/install-azure-cli.sh
+++ scripts/install-azure-cli.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# first do an update and install apt-transport-https
+apt-get update
+apt-get install -y apt-transport-https
+
+# the add keys for the MSFT apt repo
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | tee /etc/apt/sources.list.d/azure-cli.list
 apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
-apt-get install -y apt-transport-https
-# update package lists so we get the Microsoft packages (including azure-cli)
+
+# then update again and install azure-cli
 apt-get update
 apt-get install -y azure-cli
