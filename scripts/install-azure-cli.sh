3ddf4d177495afdae3316d0335f077e3d5b93c6f
Aaron Schlesinger <arschles@gmail.com>
2017-11-10 12:16:48 -0800

    Update the Microsoft apt repo receiving key (#37)
    This fixes the Azure CLI install script

--- scripts/install-azure-cli.sh
+++ scripts/install-azure-cli.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
-apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893
+# About
+#
+# This script installs the Azure CLI. It copies the steps from the following link:
+#
+# https://docs.microsoft.com/en-us/cli/azure/install-azure-cli?view=azure-cli-latest
+#
+# under the "Install with apt package manager" section
+
+# first do an update and install apt-transport-https
+apt-get update
 apt-get install -y apt-transport-https
-# update package lists so we get the Microsoft packages (including azure-cli)
+
+# the add keys for the MSFT apt repo
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | tee /etc/apt/sources.list.d/azure-cli.list
+apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
+
+# then update again and install azure-cli
 apt-get update
 apt-get install -y azure-cli
