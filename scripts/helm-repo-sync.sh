4353cd2846f94bc96582e14dd8c3f868b5eb2600
Kent Rancourt <kent.rancourt@microsoft.com>
2018-04-02 19:07:50 -0400

    Remove osba (#124)
    * remove osba chart
    
    * use pre-built tool-chain
    
    * acquire lock before updating index-- to avoid race condition

--- scripts/helm-repo-sync.sh
+++ scripts/helm-repo-sync.sh
@@ -13,9 +13,6 @@ REPO_DIR=repo
 mkdir -p $REPO_DIR
 echo "entering $REPO_DIR"
 cd $REPO_DIR
-# download the existing repo's index.yaml so that we can merge it later
-echo "downloading existing index.yaml"
-az storage blob download --container-name $AZURE_STORAGE_CONTAINER --file index.yaml --name index.yaml
 for dir in `ls ../..`;do
     if [ ! -f ../../$dir/Chart.yaml ];then
         echo "skipping $dir because it lacks a Chart.yaml file"
@@ -27,15 +24,40 @@ for dir in `ls ../..`;do
 done
 
 #####
+# upload packaged charts to Azure blob storage
+#####
+
+# AZURE_STORAGE_CONNECTION_STRING will be used for auth in the following command
+echo "uploading packaged charts from $PWD"
+az storage blob upload-batch \
+  --source . \
+  --destination $AZURE_STORAGE_CONTAINER
+
+# attempt to acquire a lock on the container
+az storage container lease acquire \
+  --container-name $AZURE_STORAGE_CONTAINER \
+  --lease-duration 60
+
+# download the existing repo's index.yaml so that we can merge it later
+echo "downloading existing index.yaml"
+az storage blob download \
+  --container-name $AZURE_STORAGE_CONTAINER \
+  --name index.yaml \
+  --file index.yaml
+
+#####
 # index the charts, merging with the old index.yaml so charts are versioned
 #####
 REPO_ROOT=https://kubernetescharts.blob.core.windows.net
 helm repo index --url "$REPO_ROOT/$AZURE_STORAGE_CONTAINER" --merge index.yaml .
 
 #####
-# upload to Azure blob storage
+# upload updated index to Azure blob storage
 #####
 
 # AZURE_STORAGE_CONNECTION_STRING will be used for auth in the following command
-echo "uploading from $PWD"
-az storage blob upload-batch --destination $AZURE_STORAGE_CONTAINER --source .
+echo "uploading updated index from $PWD"
+az storage blob upload \
+  --container-name $AZURE_STORAGE_CONTAINER \
+  --file index.yaml \
+  --name index.yaml
